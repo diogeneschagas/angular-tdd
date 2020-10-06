d35d132762872c585449367cb0080a14b328dc5d
diogeneschagas <diogeneschagas20@gmail.com>
2020-10-05 22:05:00 -0300

    Add testes de verificacao de imagens na home page

--- src/app/src/components/homes/homes.component.spec.ts
+++ src/app/src/components/homes/homes.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { spyOnClass } from 'jasmine-es6-spies';
+import { DataService } from '../../services/data.service';
 import { HomesComponent } from './homes.component';
+import { of } from 'rxjs';
 
 describe('HomesComponent', () => {
   let component: HomesComponent;
   let fixture: ComponentFixture<HomesComponent>;
+  let dataService: jasmine.SpyObj<DataService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomesComponent ]
+      declarations: [ HomesComponent ],
+      providers:[
+        { provide: DataService, useFatory: () => spyOnClass(DataService) }
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +22,49 @@ describe('HomesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomesComponent);
     component = fixture.componentInstance;
+    
+  });
+
+  beforeEach(() => {
+
+    dataService = TestBed.get(DataService);
+    dataService.getHomes$.and.returnValue(of([
+      {
+        title: 'Home 1',
+        image: 'assets/joao-pessoa.jpg',
+        location: 'João Pessoa'
+      },
+      
+      {
+        title: 'Home 2',
+        image: 'assets/joao-pessoa.jpg',
+        location: 'Fortaleza'
+      },
+      
+      {
+        title: 'Home 3',
+        image: 'assets/joao-pessoa.jpg',
+        location: 'Natal'
+      }
+    ]));
+    
     fixture.detectChanges();
+
   });
 
   it('should show homes', () => {
+    
+    
+
     expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(3);
   });
+
+  it('should show home info', () => {
+    const home = fixture.nativeElement.querySelector('[data-test="home"]');
+
+    expect(home.querySelector('[data-test="title"]').innerText).toEqual('Home 1');
+    expect(home.querySelector('[data-test="location"]').innerText).toEqual('João Pessoa');
+    expect(home.querySelector('[data-test="image"]')).toBeTruthy();
+
+  });  
 });
--- src/app/src/components/homes/homes.component.ts
+++ src/app/src/components/homes/homes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-homes',
@@ -9,29 +10,11 @@ import { of } from 'rxjs';
 export class HomesComponent implements OnInit {
 
   homes$;
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    // this.homes$ = this.dataService.getHomes$();  Aplicável apenas na utilizaçao de algum servico (API)
-    this.homes$ = of([
-      {
-        title: 'Home 1',
-        image: 'assets/joao-pessoa.jpg',
-        location: 'João Pessoa'
-      },
-      
-      {
-        title: 'Home 2',
-        image: 'assets/joao-pessoa.jpg',
-        location: 'Fortaleza'
-      },
-      
-      {
-        title: 'Home 3',
-        image: 'assets/joao-pessoa.jpg',
-        location: 'Natal'
-      }
-    ])
+    this.homes$ = this.dataService.getHomes$();  // Aplicável apenas na utilizaçao de algum servico (API)
+    
   }
 
 }
